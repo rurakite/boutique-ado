6d5164afb21fd670ea931dfaf7a12c9b374339e2
Viktor Rura <ruravictor@gmail.com>
2023-08-09 15:08:10 +0100

    Added cache controll

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -180,7 +180,7 @@ if 'USE_AWS' in os.environ:
     }
 
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'ckz8780-boutique-ado'
+    AWS_STORAGE_BUCKET_NAME = 'rurakite-demo'
     AWS_S3_REGION_NAME = 'us-east-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
